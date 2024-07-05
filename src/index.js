42b5849b3b01d430ab4e607e6c4402e2c91e7201
Aditya Saini <adityasaini@Adityas-MacBook-Air-3.local>
2024-07-05 22:16:02 +0530

    added the Airplane model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 
 const ApiRoutes = require('./routes/index');
 
+const { Airport, City, Airplane } = require('./models/index');
+const db = require('./models/index');
+const { where } = require('sequelize');
+
 const setupAndStartServer = async () => {
     const app = express();
 
@@ -13,8 +17,15 @@ const setupAndStartServer = async () => {
     app.use('/api', ApiRoutes);
     
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`The server started at ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({ alter: true });
+        }
+
+        await Airplane.create({
+            modelNumber: "Douglas DC-3"
+        })
     });
 };
 
